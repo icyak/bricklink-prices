16591ac7eed4b0d96f2ad7a33e8b691bdc473864
Peter Valachovic <valachovic.peter@gmail.com>
2023-01-13 23:13:59 +0100

    fix various pylint errors

--- bricklink.py
+++ bricklink.py
@@ -1,46 +1,50 @@
-from requests_html import HTMLSession  
+from requests_html import HTMLSession
 import pandas as pd
 
 HEADERS = {
-     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64;'
+     'x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 }
-fListOfSets = 'list_of_urls.txt'
+FLISTOFSETS= 'list_of_urls.txt'
 output =[]
-zero_items = '0 Items'
-xpath_name = '//*[@id="item-name-title"]/text()'
-xpath_items = '/html/body/div[3]/center/table/tbody/tr/td/section/div/div/div[1]/div[3]/span[1]/span/text()'
-xpath_desc = '//*[@id="_idStoreResultListSection"]/table/tbody/tr[3]/td[2]/text()'
-xpath_price = '//*[@id="_idStoreResultListSection"]/table/tbody/tr[3]/td[5]/text()'
+ZERO_ITEMS = '0 Items'
+XPATH_NAME = '//*[@id="item-name-title"]/text()'
+XPATH_ITEMS = '/html/body/div[3]/center/table/tbody/tr/td/section/div/div/div[1]/div[3]/span[1]/span/text()'
+XPATH_DESC = '//*[@id="_idStoreResultListSection"]/table/tbody/tr[3]/td[2]/text()'
+XPATH_PRICE = '//*[@id="_idStoreResultListSection"]/table/tbody/tr[3]/td[5]/text()'
 
-def render_JS_store(URL):
+def render_JS_store(url):
     session = HTMLSession()
-    response_store = session.get(URL)
+    response_store = session.get(url)
     response_store.html.render(timeout=20)
     response_store.close()
     session.close()
     return response_store
 
-def get_data_from_rendered_page2(TARGET_URL):
-    rendered_page_response = render_JS_store(TARGET_URL)
+def extract_data(target_url):
+    rendered_page_response = render_JS_store(target_url)
     try:
-        numberOfItems = rendered_page_response.html.xpath(xpath_items)
-        name = rendered_page_response.html.xpath(xpath_name)
-        if zero_items!=numberOfItems[0]:
-            description = rendered_page_response.html.xpath(xpath_desc)
-            price = rendered_page_response.html.xpath(xpath_price)[0].split()
-            items = rendered_page_response.html.xpath(xpath_items)
+        items_from_page = rendered_page_response.html.xpath(XPATH_ITEMS)
+        name = rendered_page_response.html.xpath(XPATH_NAME)
+        if ZERO_ITEMS!=items_from_page[0]:
+            description = rendered_page_response.html.xpath(XPATH_DESC)
+            price = rendered_page_response.html.xpath(XPATH_PRICE)[0].split()
+            items = rendered_page_response.html.xpath(XPATH_ITEMS)
         else:
-            numberOfItems = zero_items
-            description = rendered_page_response.html.xpath(xpath_name)
+            items_from_page = ZERO_ITEMS
+            description = rendered_page_response.html.xpath(XPATH_NAME)
             price = "[EUR, 0.0]"
             items = "[0 Items]"
     finally: None
     return name,description,price,items
 
-file = open(fListOfSets,'r')
-sets = file.readlines()
+with open(FLISTOFSETS,'r',encoding='utf-8') as file:
+    sets = file.readlines()
+
+#file = open(FLISTOFSETS,'r',encoding='utf-8')
+#sets = file.readlines()
 for set_url in sets:
-    x = get_data_from_rendered_page2(set_url)
+    x = extract_data(set_url)
     out = {
         'NAME' : x[0],
         'DESC' : x[1],
@@ -51,6 +55,6 @@ for set_url in sets:
 
 df = pd.DataFrame(output)
 print(120 * '*')
-print(df)                    # #printing the output to the command line   
+print(df)
 print(120 * '*')
 file.close()
